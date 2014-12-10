1323d300c8794fbb1462fad9f4f816374be4b772
André Roaldseth <andre@roaldseth.net>
2014-12-10 15:57:51 +0100

    Create an Erlang implementation of the SSE benchmark.

---
+++ erlang-cowboy/README.md
@@ -0,0 +1,13 @@
+# SSE implementation in Erlang + Cowboy
+
+## To build
+
+```
+$ make
+```
+
+## To run
+
+```
+$ _rel/eventsource_release/bin/eventsource_release start
+```
---
+++ erlang-cowboy/src/eventsource_app.erl
@@ -0,0 +1,23 @@
+-module(eventsource_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/sse", eventsource_emitter, []},
+            {"/connections", connections_handler, []}
+        ]}
+    ]),
+    cowboy:start_http(sse_handler, 100, [
+            {port, 8080},
+            {max_connections, infinity}
+        ],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
+    eventsource_sup:start_link().
+
+stop(_State) ->
+    ok.
