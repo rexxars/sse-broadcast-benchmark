272edc5e559a1a854aa6e37a81709d1ec25ffb95
Espen Volden <voldern@hoeggen.net>
2014-12-10 19:02:56 +0100

    Initial import of clojure-http-kit implementation

---
+++ clojure-http-kit/project.clj
@@ -0,0 +1,10 @@
+(defproject sse-server "0.0.1"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [http-kit "2.1.19"]
+                 ;; [com.ninjudd/ring-async "0.3.3"]
+                 ;; [com.ninjudd/eventual "0.4.1"]
+                 ;; [cheshire "5.4.0"]
+                 ]
+  :main sse-server.core)
