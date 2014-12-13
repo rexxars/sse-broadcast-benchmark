d6056da0639ce122e55a09c917b0849484c70f59
Espen Hovlandsdal <rexxars@gmail.com>
2014-12-13 19:56:44 +0100

    Report network timing information

--- stats-reporter/report.js
+++ stats-reporter/report.js
@@ -3,12 +3,15 @@
 
 'use strict';
 
-var spawn   = require('child_process').spawn,
+var EventSource = require('eventsource'),
+    spawn   = require('child_process').spawn,
     request = require('request'),
     fs      = require('fs'),
+    async   = require('async'),
     os      = require('os'),
     usage   = require('usage'),
     args    = require('./args')(),
+    host    = 'http://localhost:' + args.port,
     impFile = __dirname + '/../' + args.implementation + '/server',
     cmdArgs = ['--port', args.port],
     cmd     = impFile,
@@ -36,22 +39,65 @@ pid = args.pid || sse.pid;
 setInterval(gatherStats, args.probeInterval);
 
 function gatherStats() {
-    usage.lookup(sse.pid, { keepHistory: true }, function(err, result) {
+    async.parallel({
+        systemInfo: getSystemInfo,
+        networkTiming: getNetworkTiming,
+        connectionCount: getConnectionCount
+    }, function(err, results) {
         if (err) {
-            return console.error(err);
+            return;
         }
 
-        result.timestamp = Date.now();
+        reportStats(results);
+    });
+}
+
+function getSystemInfo(callback) {
+    usage.lookup(sse.pid, { keepHistory: true }, function(err, result) {
+        if (err) {
+            console.error('Failed to get system info', err);
+            return callback(err);
+        }
+
         result.loadAvg   = os.loadavg();
         result.hostname  = os.hostname();
         result.ssePort   = args.port;
         result.freeMem   = os.freemem();
 
-        reportStats(result);
+        callback(null, result);
+    });
+}
+
+function getNetworkTiming(callback) {
+    var responseTime;
+    var es = new EventSource(host + '/sse');
+    var start = Date.now();
+
+    es.onopen = function() {
+        responseTime = Date.now() - start;
+    };
+    es.onerror = function(err) {
+        console.error('Failed to request /sse when probing for response time', err);
+        es.close();
+        callback(err);
+    };
+    es.onmessage = function(msg) {
+        es.close();
+        callback(null, {
+            responseTime: responseTime,
+            deliveryTime: Date.now() - parseInt(msg.data, 10)
+        });
+    };
+}
+
+function getConnectionCount(callback) {
+    request(host + '/connections', function(err, res, body) {
+        callback(err, body | 0);
     });
 }
 
 function reportStats(stats) {
+    stats.timestamp = Date.now();
     request({
         uri: args.endpoint,
         method: 'POST',
@@ -59,7 +105,7 @@ function reportStats(stats) {
     }, onStatsReported);
 }
 
-function onStatsReported(err, res, body) {
+function onStatsReported(err, res) {
     if (err || res.statusCode >= 400) {
         return console.error('Failed reporting statistics: ', err || 'HTTP ' + res.statusCode);
     }
